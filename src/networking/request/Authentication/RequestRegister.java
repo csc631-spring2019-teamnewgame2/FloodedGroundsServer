14d8d2e265ac4534090857528911a684f9d05069
Travis <leibhaftige@hotmail.com>
2019-04-17 22:31:21 -0700

    Register protocol

--- src/networking/request/Authentication/RequestRegister.java
+++ src/networking/request/Authentication/RequestRegister.java
@@ -1,14 +1,26 @@
 package networking.request.Authentication;
 
-import java.io.IOException;
-
+import database.AccessObjectImplementation.UserDAOImpl;
+import database.AccessObjects.UserDAO;
+import database.Models.User;
+import metadata.Constants;
 import networking.request.GameRequest;
 import networking.response.Authentication.ResponseRegister;
+import utility.DataReader;
 import utility.Log;
+
+import java.io.IOException;
 
 public class RequestRegister extends GameRequest {
 
+
     private ResponseRegister responseRegister;
+    private String version;
+    private String userName;
+    private String email;
+    private String password;
+
+    private UserDAO dao = new UserDAOImpl();
 
     public RequestRegister() {
         responses.add(responseRegister = new ResponseRegister());
@@ -16,10 +28,34 @@ public class RequestRegister extends GameRequest {
 
     @Override
     public void parse() throws IOException {
+        version = DataReader.readString(dataInput).trim();
+        userName = DataReader.readString(dataInput).trim();
+        email = DataReader.readString(dataInput).trim();
+        password = DataReader.readString(dataInput).trim();
     }
 
     @Override
     public void doBusiness() throws Exception {
-        Log.println("Create Lobby Requested");
+        if (version.compareTo(Constants.CLIENT_VERSION) >= 0) {
+            if (!dao.verifyUniqueEmail(email)) {
+                Log.println("Email: '" + email + "' is already being used");
+                responseRegister.setStatus((short) 1);
+            } else if (!dao.verifyUniqueUsername(userName)) {
+                Log.println("Username: '" + userName + "' is already being used");
+                responseRegister.setStatus((short) 2);
+            } else {
+                /* TODO: do some password verification here
+                 * if password doesn't meet minimum standard,
+                 * responseRegister.setStats((short)3);
+                 */
+                responseRegister.setStatus((short) 0);
+                User user = dao.createUser(new User(userName, email, password));
+                responseRegister.setUser(user);
+            }
+            Log.println("New User Created!");
+        } else {
+            responseRegister.setStatus((short) 4); // Client version not compatible
+            Log.printf("User '%s' has failed to register. (v%s)", email, version);
+        }
     }
 }
--- src/networking/response/Authentication/ResponseRegister.java
+++ src/networking/response/Authentication/ResponseRegister.java
@@ -1,10 +1,14 @@
 package networking.response.Authentication;
 
+import database.Models.User;
 import metadata.Constants;
 import networking.response.GameResponse;
 import utility.GamePacket;
 
 public class ResponseRegister extends GameResponse {
+
+    private short status;
+    private User user;
 
     public ResponseRegister() {
         responseCode = Constants.SMSG_REGISTER;
@@ -13,6 +17,19 @@ public class ResponseRegister extends GameResponse {
     @Override
     public byte[] constructResponseInBytes() {
         GamePacket packet = new GamePacket(responseCode);
+        packet.addShort16(status);
+        if (status == 0) {
+            packet.addLong(user.getID());
+            packet.addString(user.getUserName());
+        }
         return packet.getBytes();
+    }
+
+    public void setStatus(short status) {
+        this.status = status;
+    }
+
+    public void setUser(User user){
+        this.user = user;
     }
 }
