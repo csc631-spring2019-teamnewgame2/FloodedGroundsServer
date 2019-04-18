6d907e08e272594753ae5898a1cda30fd0c15533
Travis <leibhaftige@hotmail.com>
2019-04-17 19:19:14 -0700

    More Test code

--- test/LobbyTest.java
+++ test/LobbyTest.java
@@ -3,4 +3,29 @@ import database.Models.Lobby;
 public class LobbyTest {
     private Lobby lobby;
 
+
+    public boolean testGetLobbyByID() {
+        return false;
+    }
+    public boolean testGetAllLobbies(){
+        return false;
+    }
+    public boolean testGetLobbyByName(){
+        return false;
+    }
+    public boolean testGetLobbyByOwner() {
+        return false;
+    }
+    public boolean testCreateLobby(){
+        return false;
+    }
+    public boolean testAddPlayerToLobby(){
+        return false;
+    }
+    public boolean testDeleteLobby(){
+        return false;
+    }
+
+
+
 }
