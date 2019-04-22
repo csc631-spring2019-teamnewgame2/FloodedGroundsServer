b63e4434fd37eec4670e5bc71cce062e691effca
Travis <leibhaftige@hotmail.com>
2019-04-21 17:50:35 -0700

    Server side of lobby list retrieval

--- src/networking/request/Lobby/RequestGetLobbies.java
+++ src/networking/request/Lobby/RequestGetLobbies.java
@@ -1,14 +1,28 @@
 package networking.request.Lobby;
 
-import java.io.IOException;
-
+import database.AccessObjectImplementation.LobbyDAOImpl;
+import database.AccessObjectImplementation.UserDAOImpl;
+import database.AccessObjects.LobbyDAO;
+import database.AccessObjects.UserDAO;
+import database.Models.Lobby;
+import metadata.Constants;
 import networking.request.GameRequest;
 import networking.response.Lobby.ResponseGetLobbies;
+import utility.DataReader;
 import utility.Log;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 public class RequestGetLobbies extends GameRequest {
 
     private ResponseGetLobbies responseGetLobbies;
+    private LobbyDAO lobbyDAO = new LobbyDAOImpl();
+    private UserDAO userDAO = new UserDAOImpl();
+    private String version;
+    private List<Lobby> lobbyList;
+    private Map<Long, String> usernames;
 
     public RequestGetLobbies() {
         responses.add(responseGetLobbies = new ResponseGetLobbies());
@@ -16,10 +30,22 @@ public class RequestGetLobbies extends GameRequest {
 
     @Override
     public void parse() throws IOException {
+        version = DataReader.readString(dataInput).trim();
     }
 
     @Override
     public void doBusiness() throws Exception {
-        Log.println("Get Lobbies Requested");
+        if (version.compareTo(Constants.CLIENT_VERSION) >= 0) {
+            Log.println("Get Lobbies Requested");
+
+            lobbyList = lobbyDAO.getAllLobbies();
+            if (lobbyList.size() > 0) {
+                responseGetLobbies.setStatus((short) 0);
+                responseGetLobbies.setLobbyList(lobbyList);
+                responseGetLobbies.setUserNames(userDAO.getAllUsernames());
+            } else {
+                responseGetLobbies.setStatus((short) 1);
+            }
+        }
     }
 }
--- src/networking/response/Lobby/ResponseGetLobbies.java
+++ src/networking/response/Lobby/ResponseGetLobbies.java
@@ -1,8 +1,12 @@
 package networking.response.Lobby;
 
+import database.Models.Lobby;
 import metadata.Constants;
 import networking.response.GameResponse;
 import utility.GamePacket;
+
+import java.util.List;
+import java.util.Map;
 
 public class ResponseGetLobbies extends GameResponse {
 
@@ -10,9 +14,34 @@ public class ResponseGetLobbies extends GameResponse {
         responseCode = Constants.SMSG_GETLOBBIES;
     }
 
+    short status;
+    int size;
+    List<Lobby> lobbyList;
+    Map<Long,String> userNames;
     @Override
     public byte[] constructResponseInBytes() {
         GamePacket packet = new GamePacket(responseCode);
+        packet.addShort16(status);
+
+
+        if(status == 0) {
+            Lobby lobby = null;
+            packet.addInt32(size);
+            for (int i = 0; i < lobbyList.size(); i++) {
+                lobby = lobbyList.get(i);
+                packet.addLong(lobby.getID());
+                packet.addString(lobby.getName());
+                packet.addString(userNames.get(lobby.getOwner()));
+                packet.addInt32(lobby.getPrivacy());
+                packet.addBoolean(lobby.getPasswordRequired());
+            }
+        }
         return packet.getBytes();
     }
+
+    public void setStatus(short status){this.status = status;}
+
+    public void setLobbyList(List<Lobby> lobbyList){ this.lobbyList.addAll(lobbyList);}
+
+    public void setUserNames(Map<Long,String> userNames){ this.userNames = userNames;}
 }
