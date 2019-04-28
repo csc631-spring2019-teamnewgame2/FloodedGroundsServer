1f60f7dfdf0a19d64311f1969a19f4b7466f8091
Travis <leibhaftige@hotmail.com>
2019-04-28 15:16:26 -0700

    Join lobby protocol implemented

--- src/networking/response/Lobby/ResponseJoinLobby.java
+++ src/networking/response/Lobby/ResponseJoinLobby.java
@@ -1,5 +1,6 @@
 package networking.response.Lobby;
 
+import database.Models.Lobby;
 import metadata.Constants;
 import networking.response.GameResponse;
 import utility.GamePacket;
@@ -10,9 +11,30 @@ public class ResponseJoinLobby extends GameResponse {
         responseCode = Constants.SMSG_JOINLOBBY;
     }
 
+    private short status;
+    private long lobbyID;
+    private int port;
+
     @Override
     public byte[] constructResponseInBytes() {
         GamePacket packet = new GamePacket(responseCode);
+        packet.addShort16(status);
+        if (status == 0) {
+            packet.addLong(lobbyID);
+            packet.addInt32(port);
+        }
         return packet.getBytes();
+    }
+
+    public void setStatus(short status) {
+        this.status = status;
+    }
+
+    public void setLobbyID(long lobbyID) {
+        this.lobbyID = lobbyID;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
     }
 }
